7ab8a9a471022730c97218a6e833531494c64a31
Luiz Henrique <presidente@masterlantelecom.com.br>
2022-09-02 22:37:04 -0300

    Script hide all items and only display one

--- carousel.js
+++ carousel.js
@@ -1,7 +1,11 @@
+// Constants definition
 const carousel = document.getElementById('carousel');
-
 const items = carousel.querySelectorAll('.item');
+const countItems = 0;
 
+// Selects items one-by-one
 for (i of items){
-    
+    i.classList.add('hide');
 }
+
+items[countItems].classList.remove('hide');
