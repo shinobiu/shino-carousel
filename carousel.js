3ed55e95d8ad43ef8977f59704c38407a368a155
Luiz Henrique <presidente@masterlantelecom.com.br>
2022-09-03 01:17:13 -0300

    Completed Project for now

--- carousel.js
+++ carousel.js
@@ -1,11 +1,74 @@
 // Constants definition
 const carousel = document.getElementById('carousel');
+const move = document.getElementById('move');
 const items = carousel.querySelectorAll('.item');
-const countItems = 0;
+let countItems = 0;
+const bottomBar = document.createElement('div');
+let itemID = [];
 
-// Selects items one-by-one
-for (i of items){
-    i.classList.add('hide');
+start();
+
+function start() {
+    let count = 0
+    for (i of items) {
+        i.classList.add('hide');
+        createIdentifiers(count);
+        count ++;
+    }
+    items[countItems].classList.remove('hide');
+    itemID[countItems].style.opacity = '100%'
+    bottomBar.classList = 'bottomBar';
+    carousel.appendChild(bottomBar);
+    move.addEventListener('click', (e) => {
+        const direction = e.target.innerText;
+        changeItem(direction);
+    })
+    bottomBar.addEventListener('click', (e) => {
+        const direction = e.target.id;
+        if(!direction) {
+            return
+        }
+        goTo(direction);
+    })
 }
 
-items[countItems].classList.remove('hide');
+function createIdentifiers(idNumber) {
+    const identifier = document.createElement('div');
+    identifier.classList = 'identifier';
+    identifier.id = idNumber;
+    bottomBar.appendChild(identifier);
+    itemID.push(identifier);
+}
+
+function changeItem(direction) {
+    if (direction === 'arrow_forward_ios') {
+        items[countItems].classList.add('hide');
+        itemID[countItems].style.opacity = '50%';
+        if (countItems >= items.length - 1) {
+            countItems = 0
+        } else {
+            countItems++
+        }
+        items[countItems].classList.remove('hide');
+        itemID[countItems].style.opacity = '100%';
+    }
+    if (direction === 'arrow_back_ios') {
+        items[countItems].classList.add('hide');
+        itemID[countItems].style.opacity = '50%';
+        if (countItems === 0) {
+            countItems = items.length - 1
+        } else {
+            countItems--
+        }
+        items[countItems].classList.remove('hide');
+        itemID[countItems].style.opacity = '100%';
+    }
+}
+
+function goTo (direction) {
+    items[countItems].classList.add('hide');
+    itemID[countItems].style.opacity = '50%';
+    countItems = direction;
+    items[countItems].classList.remove('hide');
+    itemID[countItems].style.opacity = '100%';
+}
